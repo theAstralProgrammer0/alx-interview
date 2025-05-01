6e76ac8f91dfaa600369779bf3494827c2ec1416
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-05-01 02:31:52 +0100

    Added documentation and PEP3 styling

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,13 +1,32 @@
+"""This module finds the minimum number of operations of performing a task
+"""
+
+
 def isPrime(n):
+    """This function finds the prime value of a number.
+
+        Args:
+            n => number
+        Return:
+            Bool
+    """
     for num in range(2, n):
         if n % num == 0:
             return False
     return True
 
-def minOperations(n):
-    # Initialization 
+
+def primeFactors(n):
+    """This function finds the prime factors of a number
+
+        Args:
+            n => number
+        Return:
+            factors => List of Prime Factors (Integers)
+    """
+    # Initialization
     factors = []
-    
+
     # Iteration
     while n != 1:
         for num in range(2, n + 1):
@@ -15,5 +34,15 @@ def minOperations(n):
                 factors.append(num)
                 n //= num
                 break
-    return sum(factors)
+    return factors
 
+
+def minOperations(n):
+    """This function finds the minimum number of operations
+
+        Args:
+            n => task magnitude
+        Return:
+            Integer => Sum of Prime Factors
+    """
+    return sum(primeFactors(n))
